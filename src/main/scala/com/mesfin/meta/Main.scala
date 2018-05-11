1a096e3165a24e72b6bde27a5faac3be433ae57c
Mesfin Mebrate <mesfin.mebrate@basementcrowd.com>
2018-05-11 17:36:55 +0100

    Playing around with the meta template and parsing code.

---
+++ src/main/scala/com/mesfin/meta/Main.scala
@@ -0,0 +1,32 @@
+package com.mesfin.meta
+
+import scala.meta._
+
+object Main extends App {
+  val code =
+    """case class Car[CarCompany](brand: CarCompany, color: Color, name: String){
+         val owner: String = "John"
+         def playRadio() = {
+           "playing radio"
+         }
+         val capacity, speed = (5, 200)
+         val oneVal = 45
+      }
+    """.parse[Stat]
+
+  val q"..$mods class $tname[..$tparams] ..$mods2 (...$paramss) extends $template" = parseCode(code)
+
+  template match {
+    case template"{ ..$stats } with ..$ctorcalls { $param => ..$stats2 }" => stats2.map {
+      case q"..$mods def $name[..$tparams](...$paramss): $tpe = $expr" => println(s"methodName: $name")
+      case q"..$mods val ..$patsnel: $tpeopt = $expr" => println(s"value $patsnel equals to $expr")
+    }
+  }
+
+  def parseCode[T](code: Parsed[T]): T = {
+    code match {
+      case Parsed.Success(tree) => tree
+      case Parsed.Error(pos, msg, details)  => throw new Exception(msg)
+    }
+  }
+}
