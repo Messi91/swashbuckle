bf59baa9c5b5170600fdd1ca03a5d19201af7a85
Mesfin Mebrate <mesfin.mebrate@basementcrowd.com>
2018-07-09 17:50:45 +0100

    Creating a Swagger JSON representation of the ServerDocumentation.

--- src/main/scala/com/swashbuckle/json/SwashbuckleJsonSupport.scala
+++ src/main/scala/com/swashbuckle/json/SwashbuckleJsonSupport.scala
@@ -8,74 +8,112 @@ import spray.json.{JsString, _}
 
 trait SwashbuckleJsonSupport {
 
-  private def translateType(`type`: PathParameterType): (String, Option[String]) = `type` match {
-    case PathParameterTypes.IntNumber => ("integer", Some("int32"))
-    case PathParameterTypes.LongNumber => ("integer", Some("int64"))
-    case PathParameterTypes.DoubleNumber => ("number", Some("double"))
-    case PathParameterTypes.JavaUUID => ("string", Some("uuid"))
-  }
-
-  private def translateType(`type`: PrimitiveType): (String, Option[String]) = `type` match {
-    case PrimitiveTypes.IntType => ("integer", Some("int32"))
-    case PrimitiveTypes.LongType => ("integer", Some("int64"))
-    case PrimitiveTypes.DoubleType => ("number", Some("double"))
-    case PrimitiveTypes.StringType => ("string", None)
-    case PrimitiveTypes.BooleanType => ("boolean", None)
-  }
-
-//  {
-//    "name":"tags",
-//    "in":"query",
-//    "description":"Tags to filter by",
-//    "required":true,
-//    "type":"array",
-//    "items":{
-//      "type":"string"
-//    },
-//    "collectionFormat":"multi"
-//  }
-
   implicit object ParameterFormat extends JsonWriter[Parameter] {
     override def write(parameter: Parameter): JsValue = parameter match {
       case PathParameter(name, parameterType) =>
-        val (typeName, formatOpt) = translateType(parameterType)
-        val default = JsObject(
-          "name" -> JsString(name),
-          "in" -> JsString("path"),
-          "required" -> JsBoolean(true),
-          "type" -> JsString(typeName)
+        val (typeName, format) = translatePathParameterType(parameterType)
+        addFormatField(
+          JsObject(
+            "name" -> JsString(name),
+            "in" -> JsString("path"),
+            "required" -> JsBoolean(true),
+            "type" -> JsString(typeName)
+          ),
+          format
         )
-        formatOpt match {
-          case Some(format) => default.copy(fields = default.fields + ("format" -> JsString(format)))
-          case None => default
-        }
 
       case QueryParameter(name, primitiveType, required) =>
-        val (typeName, formatOpt) = translateType(primitiveType)
-        val default = JsObject(
-          "name" -> JsString(name),
-          "in" -> JsString("query"),
-          "required" -> JsBoolean(required),
-          "type" -> JsString(typeName)
+        val (typeName, format) = translatePrimitiveType(primitiveType)
+        addFormatField(
+          JsObject(
+            "name" -> JsString(name),
+            "in" -> JsString("query"),
+            "required" -> JsBoolean(required),
+            "type" -> JsString(typeName)
+          ),
+          format
         )
-        formatOpt match {
-          case Some(format) => default.copy(fields = default.fields + ("format" -> JsString(format)))
-          case None => default
-        }
 
-      case ArrayQueryParameter()
+      case ArrayQueryParameter(name, primitiveType, collectionFormat, required) =>
+        val (typeName, format) = translatePrimitiveType(primitiveType)
+        addFormatField(
+          JsObject(
+            "name" -> JsString(name),
+            "in" -> JsString("query"),
+            "required" -> JsBoolean(required),
+            "type" -> JsString("array"),
+            "items" -> JsObject(
+              "type" -> JsString(typeName)
+            ),
+            "collectionFormat" -> JsString(collectionFormat)
+          ),
+          format
+        )
+
+      case BodyParameter(name, schemaName) =>
+        JsObject(
+          "in" -> JsString("body"),
+          "name" -> JsString(name),
+          "required" -> JsBoolean(true),
+          "schema" -> JsObject(
+            "$ref" -> JsString(tagReference(schemaName))
+          )
+        )
     }
   }
 
   implicit object ResponseFormat extends JsonWriter[Response] {
-    override def write(obj: Response): JsValue = {
-      ???
+    override def write(response: Response): JsValue = response match {
+      case Response(status, Some(message), None, _) =>
+        JsObject(
+          status.toString -> JsObject(
+            "description" -> JsString(message)
+          )
+        )
+
+      case Response(status, None, Some(schemaName), false) =>
+        JsObject(
+          status.toString -> JsObject(
+            "description" -> JsString("successful operation"),
+            "schema" -> JsObject(
+              "$ref" -> JsString(tagReference(schemaName))
+            )
+          )
+        )
+
+      case Response(status, None, Some(schemaName), true) =>
+        JsObject(
+          status.toString -> JsObject(
+            "description" -> JsString("successful operation"),
+            "schema" -> JsObject(
+              "type" -> JsString("array"),
+              "items" -> JsObject(
+                "$ref" -> JsString(tagReference(schemaName))
+              )
+            )
+          )
+        )
     }
   }
 
   implicit object PathFormat extends JsonWriter[Path] {
-    override def write(obj: Path): JsValue = {
-      ???
+    override def write(path: Path): JsValue = {
+      JsObject(
+        path.url.mkString("/") -> JsObject(
+          translateMethod(path.method) -> JsObject(
+            "operationId" -> JsString(path.name),
+            "produces" -> JsArray(
+              JsString("application/json")
+            ),
+            "parameters" -> JsArray(
+              path.parameters.map(_.toJson).toVector
+            ),
+            "responses" -> JsArray(
+              path.responses.map(_.toJson).toVector
+            )
+          )
+        )
+      )
     }
   }
 
@@ -90,4 +128,35 @@ trait SwashbuckleJsonSupport {
       ???
     }
   }
+
+  private def translatePathParameterType(`type`: PathParameterType): (String, Option[String]) = `type` match {
+    case PathParameterTypes.IntNumber => ("integer", Some("int32"))
+    case PathParameterTypes.LongNumber => ("integer", Some("int64"))
+    case PathParameterTypes.DoubleNumber => ("number", Some("double"))
+    case PathParameterTypes.JavaUUID => ("string", Some("uuid"))
+  }
+
+  private def translatePrimitiveType(`type`: PrimitiveType): (String, Option[String]) = `type` match {
+    case PrimitiveTypes.IntType => ("integer", Some("int32"))
+    case PrimitiveTypes.LongType => ("integer", Some("int64"))
+    case PrimitiveTypes.DoubleType => ("number", Some("double"))
+    case PrimitiveTypes.StringType => ("string", None)
+    case PrimitiveTypes.BooleanType => ("boolean", None)
+  }
+
+  private def translateMethod(method: Method): String = method match {
+    case Get => "get"
+    case Post => "post"
+    case Put => "put"
+    case Delete => "delete"
+  }
+
+  private def addFormatField(json: JsObject, format: Option[String]) = {
+    format match {
+      case Some(fmt) => json.copy(fields = json.fields + ("format" -> JsString(fmt)))
+      case None => json
+    }
+  }
+
+  private def tagReference(schemaName: String): String = s"#/definitions/$schemaName"
 }
