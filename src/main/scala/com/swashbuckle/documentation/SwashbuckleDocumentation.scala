0f3c6d96d679dcdb7602a4adf1b398754c0c9c93
Mesfin Mebrate <mesfin.mebrate@basementcrowd.com>
2018-07-12 21:19:39 +0100

    Testing the documentation on Swagger UI and adding CORS support.

--- src/main/scala/com/swashbuckle/documentation/SwashbuckleDocumentation.scala
+++ src/main/scala/com/swashbuckle/documentation/SwashbuckleDocumentation.scala
@@ -4,14 +4,15 @@ import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.server.{Directives, Route}
 import com.swashbuckle.config.SwashbuckleConfigComponent
 import com.swashbuckle.json.SwashbuckleJsonSupport
+import com.swashbuckle.service.CorsSupport
 import spray.json._
 
-trait SwashbuckleDocumentation extends SwashbuckleConfigComponent with Directives with SwashbuckleJsonSupport {
-  def documentationRoute: Route = pathPrefix("documentation") {
-    pathEndOrSingleSlash {
+trait SwashbuckleDocumentation extends SwashbuckleConfigComponent with Directives with SwashbuckleJsonSupport with CorsSupport {
+  val documentationRoute: Route = pathPrefix("documentation") {
+    corsHandler(pathEndOrSingleSlash {
       get {
         complete(ServerDocumentation(swashbuckleConfig).toJson.prettyPrint)
       }
-    }
+    })
   }
 }
